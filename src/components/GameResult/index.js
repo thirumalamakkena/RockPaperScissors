35fc4972914b345b87e0c91e0e446a9377f02f89
thirumalamakkena <thirumalathiru2003@gmail.com>
2023-10-09 13:31:28 +0000

    commited

---
+++ src/components/GameResult/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const GameResult = props => {
+  const {onClickPlayAgain, optedChoice, opponentChoice, result} = props
+
+  const onClickButton = () => {
+    onClickPlayAgain()
+  }
+
+  return (
+   
+  )
+}
+
+export default GameResult
